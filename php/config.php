0e92dd9dd5b00c991364156e756b9ed9a9b69c48
Jean <jeanncavalcante@gmail.com>
2022-08-13 15:21:05 -0300

    mysql

--- php/config.php
+++ php/config.php
@@ -1,8 +1,15 @@
 <?php
 
-$db_name = 'test';
-$db_host = 'localhost';
-$db_user = 'root';
-$db_pass = '';
+// $db_name = 'test';
+// $db_host = 'localhost';
+// $db_user = 'root';
+// $db_pass = '';
+
+$db_name = 'b3c0fe695717eb';
+$db_host = '7641be3f';
+$db_user = 'us-cdbr-east-06.cleardb.net';
+$db_pass = 'heroku_e2512654187de92';
 
 $pdo = new PDO("mysql:dbname=$db_name;host=$db_host", $db_user, $db_pass);
+
+//mysql://b3c0fe695717eb:7641be3f@us-cdbr-east-06.cleardb.net/heroku_e2512654187de92?reconnect=tru
